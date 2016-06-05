04b853a5f42ff22a03f5a90a3c1926ab19bff0d4
cooper <lyt3131@163.com>
2016-06-06 05:37:51 +0800

    tt

--- src/main/java/cc/coopersoft/restaurant/HttpSecurityConfiguration.java
+++ src/main/java/cc/coopersoft/restaurant/HttpSecurityConfiguration.java
@@ -14,13 +14,13 @@ public class HttpSecurityConfiguration {
         SecurityConfigurationBuilder builder = event.getBuilder();
 
         builder.http()
-                .allPaths()
-                    .authenticateWith()
-                    .form()
-                    .authenticationUri("/login.xhtml")
-                    .loginPage("/login.xhtml")
-                    .errorPage("/error.xhtml")
-                    .restoreOriginalRequest()
+//                .allPaths()
+//                    .authenticateWith()
+//                    .form()
+//                    .authenticationUri("/login.xhtml")
+//                    .loginPage("/login.xhtml")
+//                    .errorPage("/error.xhtml")
+//                    .restoreOriginalRequest()
                 .forPath("/logout")
                     .logout()
                     .redirectTo("/index.html");
